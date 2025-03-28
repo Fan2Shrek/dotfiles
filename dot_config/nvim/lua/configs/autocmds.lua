c45eaabe0fdc78e122090c14a46ece2d2c36f05a
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-03-28 14:34:36 +0100

    Remove kitty padding

--- dot_config/nvim/lua/configs/autocmds.lua
+++ dot_config/nvim/lua/configs/autocmds.lua
@@ -2,13 +2,13 @@ vim.api.nvim_create_autocmd("BufDelete", {
 	callback = function()
 		local bufs = vim.t.bufs
 		if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-			Snacks.dashboard({example = "advanced"})
+			Snacks.dashboard({ example = "advanced" })
 		end
 	end,
 })
 
 vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
-        require("nvim-tree.api").tree.close()
-    end,
+	callback = function()
+		require("nvim-tree.api").tree.close()
+	end,
 })
